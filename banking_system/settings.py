43b89cdc8aae2d050b04dad453f303c5ea6485ab
Yukesh Choudhary <157104816+uknation@users.noreply.github.com>
2025-06-10 13:52:08 +0530

    settings.py

--- banking_system/settings.py
+++ banking_system/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'po0172$69b@78ps4v^uhfxu6q--8ko7kpp7rbz420s_3w#sir%'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [https://online-banking-system-7ksn.onrender.com]
+ALLOWED_HOSTS = ['online-banking-system-7ksn.onrender.com']
+
 
 
 # Application definition
